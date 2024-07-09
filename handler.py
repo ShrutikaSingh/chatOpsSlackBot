cb1cad47f21e1a2d5046999295e93ec3f351e387
ShrutikaSingh <shrutika051220@gmail.com>
2024-07-09 09:25:45 -0700

    add dropdown for different aws services for users to select and perform actions dropdown and parameters dialogue box"

--- handler.py
+++ handler.py
@@ -1,38 +1,13 @@
 import json
 import boto3
-import urllib3
 import os
+import urllib3
 from urllib.parse import parse_qs
 
-# Initialize the AWS Lambda client
-client = boto3.client('lambda')
 http = urllib3.PoolManager()
 
 with open('command_config.json', 'r') as f:
     config = json.load(f)
-
-commands = config.get('commands', {})
-print(commands, "commands")
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!"
-    }
-    return {"statusCode": 200, "body": json.dumps(body)}
-
-def parse_slack_command(message):
-    message_lower = message.lower()
-    for cmd, cmd_data in commands.items():
-        if any(keyword in message_lower for keyword in cmd.split()):
-            return cmd, cmd_data
-    return None, None
-
-def invoke_lambda(function_name, event):
-    return client.invoke(
-        FunctionName=function_name,
-        InvocationType='RequestResponse',
-        Payload=json.dumps(event)
-    )
 
 def respond(message, extra_data=None):
     response = {
@@ -43,135 +18,182 @@ def respond(message, extra_data=None):
         response['extra_data'] = extra_data
     return response
 
-def first_lambda(event, context):
-
-    body = parse_qs(event['body'])
-    
-    if 'command' not in body:
-        return respond("Error: command key not found in event data")
-    command_text = body['command'][0]
-
-    print("Parsed command is:", command_text)
-    command, cmd_data = parse_slack_command(command_text)
-
-    if command:
-        function_name = cmd_data['function']
-        response = invoke_lambda(function_name, event)
-        return respond(f"Successfully triggered /{command}!")
-    else:
-        return respond(f"Unrecognized command: /{command_text}")
-
-def second_lambda(event, context):
-    print('invoked hello lambda')
-    message = commands['hello']['message']
-    send_to_slack(message)
-    return respond(message)
-
 def third_lambda(event, context):
-    print('invoked bye lambda')
-
     if 'body' not in event:
         return respond("Error: Body not found in event data")
-    
-    body = parse_qs(event['body'])
 
-    if 'payload' in body:
-        payload = json.loads(body['payload'][0])
-        if payload['type'] == 'dialog_submission':
-            submission = payload['submission']
-            user_to = submission.get('to', 'Default User')
-            region = submission.get('region', 'Default Region')
-            additional_message = submission.get('additional_message', 'No additional message')
-
-            print('printing', 'user_to', user_to, 'region', region, 'additional_message', additional_message)
-
-            message = f"Goodbye from Third Lambda! to {user_to} region: {region} additional message: {additional_message}"
-            send_to_slack(message)
-            return respond(message)
+    try:
+        if event['headers'].get('Content-Type') == 'application/json':
+            body = json.loads(event['body'])
         else:
-            return respond("Error: Unsupported payload type")
+            body = parse_qs(event['body'])
 
-    if 'command' not in body:
-        return respond("Error: command key not found in event data")
-    
-    command_text = body['command'][0]
-
-    if command_text == '/bye':
-        if 'trigger_id' not in body:
-            return respond("Error: trigger_id not found in event data")
-        
-        trigger_id = body['trigger_id'][0]
-        dialog = {
-            "trigger_id": trigger_id,
-            "dialog": {
-                "callback_id": "bye_dialog",
-                "title": "Goodbye Details",
-                "submit_label": "Submit",
-                "elements": [
-                    {
-                        "type": "text",
-                        "label": "To",
-                        "name": "to",
-                        "placeholder": "Enter recipient"
-                    },
-                    {
-                        "type": "text",
-                        "label": "Region",
-                        "name": "region",
-                        "placeholder": "Enter region"
-                    },
-                    {
-                        "type": "textarea",
-                        "label": "Additional Message",
-                        "name": "additional_message",
-                        "optional": True
-                    }
-                ]
+        if isinstance(body, dict) and body.get('type') == 'url_verification':
+            return {
+                'statusCode': 200,
+                'body': body['challenge']
             }
+
+        if 'payload' in body:
+            payload = json.loads(body['payload'][0])
+            if payload['type'] == 'interactive_message':
+                # Handle dropdown selections
+                selected_service = payload['actions'][0]['selected_options'][0]['value']
+                trigger_id = payload['trigger_id']
+                return open_action_dropdown(trigger_id, selected_service)
+            elif payload['type'] == 'dialog_submission':
+                submission = payload['submission']
+                service = payload['callback_id'].split('_')[0]
+                action = payload['callback_id'].split('_')[1]
+                params = {key: value for key, value in submission.items()}
+                return perform_action(service, action, params)
+            else:
+                return respond("Error: Unsupported payload type")
+
+        if 'command' in body:
+            command_text = body['command'][0]
+
+            if command_text == '/aws':
+                if 'trigger_id' not in body:
+                    return respond("Error: trigger_id not found in event data")
+
+                trigger_id = body['trigger_id'][0]
+                return open_service_dropdown(trigger_id)
+
+            return respond(f"Unrecognized command: {command_text}")
+
+        return respond("Error: Invalid request format")
+
+    except Exception as e:
+        return respond(f"Error: {str(e)}")
+
+def open_service_dropdown(trigger_id):
+    services = list(config['services'].keys())
+    dialog = {
+        "trigger_id": trigger_id,
+        "dialog": {
+            "callback_id": "select_service",
+            "title": "Select AWS Service",
+            "elements": [
+                {
+                    "type": "select",
+                    "label": "Service",
+                    "name": "service",
+                    "options": [{"label": service, "value": service} for service in services]
+                }
+            ]
         }
-
-        if 'SLACK_BOT_TOKEN' not in os.environ:
-            return respond("Error: SLACK_BOT_TOKEN environment variable not set")
-
-        print("Dialog Payload:", json.dumps(dialog))
-
-        slack_response = http.request(
-            'POST',
-            'https://slack.com/api/dialog.open',
-            body=json.dumps(dialog).encode('utf-8'),
-            headers={
-                'Content-Type': 'application/json',
-                'Authorization': f"Bearer {os.environ['SLACK_BOT_TOKEN']}"
-            }
-        )
-
-        print("Slack Response:", slack_response.status, slack_response.data)
-
-        if slack_response.status != 200 or json.loads(slack_response.data.decode('utf-8')).get('ok') is not True:
-            return respond(f"Error: Unable to open dialog - {slack_response.data}")
-
-        return respond("Dialog sent to Slack!")
-
-    return respond(f"Unrecognized command: {command_text}")
-
-def send_to_slack(message):
-    slack_webhook_url = os.environ["SLACK_WEBHOOK_URL"]
-    message = {
-        'text': message
     }
-    encoded_data = json.dumps(message).encode('utf-8')
-    
-    response = http.request(
+
+    return send_dialog_to_slack(dialog)
+
+def open_action_dropdown(trigger_id, service):
+    actions = list(config['services'][service]['actions'].keys())
+    dialog = {
+        "trigger_id": trigger_id,
+        "dialog": {
+            "callback_id": f"{service}_select_action",
+            "title": f"Select Action for {service}",
+            "elements": [
+                {
+                    "type": "select",
+                    "label": "Action",
+                    "name": "action",
+                    "options": [{"label": action, "value": action} for action in actions]
+                }
+            ]
+        }
+    }
+
+    return send_dialog_to_slack(dialog)
+
+def open_action_parameters_dialog(trigger_id, service, action):
+    parameters = config['services'][service]['actions'][action]['parameters']
+    dialog_elements = [
+        {"type": "text", "label": key, "name": key, "value": value} for key, value in parameters.items()
+    ]
+    dialog = {
+        "trigger_id": trigger_id,
+        "dialog": {
+            "callback_id": f"{service}_{action}",
+            "title": f"Parameters for {service} {action}",
+            "elements": dialog_elements
+        }
+    }
+
+    return send_dialog_to_slack(dialog)
+
+def send_dialog_to_slack(dialog):
+    if 'SLACK_BOT_TOKEN' not in os.environ:
+        return respond("Error: SLACK_BOT_TOKEN environment variable not set")
+
+    slack_response = http.request(
         'POST',
-        slack_webhook_url,
-        body=encoded_data,
-        headers={'Content-Type': 'application/json'}
+        'https://slack.com/api/dialog.open',
+        body=json.dumps(dialog).encode('utf-8'),
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {os.environ['SLACK_BOT_TOKEN']}"
+        }
     )
-   
-    if response.status != 200:
-        raise Exception(f"Request to Slack returned an error {response.status}, the response is:\n{response.data}")
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Lambda finished and notified Slack!')
-    }
+    print("Slack Response Status:", slack_response.status)
+    print("Slack Response Data:", slack_response.data.decode('utf-8'))
+
+    if slack_response.status != 200 or json.loads(slack_response.data.decode('utf-8')).get('ok') is not True:
+        return respond(f"Error: Unable to open dialog - {slack_response.data}")
+
+    return respond("Dialog sent to Slack!")
+
+def perform_action(service, action, params):
+    if service == "ec2":
+        return handle_ec2_action(action, params)
+    elif service == "s3":
+        return handle_s3_action(action, params)
+    elif service == "iam":
+        return handle_iam_action(action, params)
+    else:
+        return respond(f"Unsupported service: {service}")
+
+def handle_ec2_action(action, params):
+    ec2 = boto3.client('ec2')
+    if action == "create":
+        response = ec2.run_instances(
+            ImageId=params['ami'],
+            InstanceType=params['instance_type'],
+            KeyName=params['key_name'],
+            MaxCount=1,
+            MinCount=1,
+            Placement={'AvailabilityZone': params['region']}
+        )
+    elif action == "start":
+        response = ec2.start_instances(InstanceIds=[params['instance_id']])
+    elif action == "stop":
+        response = ec2.stop_instances(InstanceIds=[params['instance_id']])
+    elif action == "terminate":
+        response = ec2.terminate_instances(InstanceIds=[params['instance_id']])
+    else:
+        return respond(f"Unsupported EC2 action: {action}")
+
+    return respond(response)
+
+def handle_s3_action(action, params):
+    s3 = boto3.client('s3')
+    if action == "create":
+        response = s3.create_bucket(
+            Bucket=params['bucket_name'],
+            CreateBucketConfiguration={'LocationConstraint': params['region']}
+        )
+    else:
+        return respond(f"Unsupported S3 action: {action}")
+
+    return respond(response)
+
+def handle_iam_action(action, params):
+    iam = boto3.client('iam')
+    if action == "create_user":
+        response = iam.create_user(UserName=params['user_name'])
+    else:
+        return respond(f"Unsupported IAM action: {action}")
+
+    return respond(response)
